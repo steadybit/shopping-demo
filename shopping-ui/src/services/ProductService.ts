c01768f1593dbe540b0e2bfd2839b4d3fb5ae8db
Johannes Edmeier <johannes.edmeier@steadybit.com>
2021-05-28 16:46:57 +0200

    fix: set timeout for axios

--- shopping-ui/src/services/ProductService.ts
+++ shopping-ui/src/services/ProductService.ts
@@ -3,11 +3,14 @@ import { ProductsCircuitBreaker } from './productsCircuitBreaker';
 import { ProductsParallel } from './productsParallel';
 import { ProductsExceptionHandling } from './productsExceptionHandling';
 import { ProductsTimeoutHandling } from './productsTimeoutHandling';
+import axios from 'axios';
+
+axios.defaults.timeout = 3000;
 
 export const ProductService = {
     legacy: new ProductsLegacy(),
     parallel: new ProductsParallel(),
     circuitBreaker: new ProductsCircuitBreaker(),
     exceptionHandling: new ProductsExceptionHandling(),
-    timeoutHandling: new ProductsTimeoutHandling(),
+    timeoutHandling: new ProductsTimeoutHandling()
 };
