65ef38cef1ee1b4e8d1ba53daadc83f1417c99ff
Daniel Reuter <1879911+ReuDa@users.noreply.github.com>
2021-10-14 16:21:37 +0200

    feat: refactored back to old structure to better match existing blog posts

--- gateway/src/main/java/com/steadybit/demo/shopping/gateway/ProductsController.java
+++ gateway/src/main/java/com/steadybit/demo/shopping/gateway/ProductsController.java
@@ -9,14 +9,21 @@ import com.steadybit.shopping.domain.Products;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
 
@@ -24,8 +31,13 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductsController {
     private static final Logger log = LoggerFactory.getLogger(GatewayApplication.class);
-
-    private final ProductService productService;
+    private final RestTemplate restTemplateWithoutTimeout;
+    private final RestTemplate restTemplate;
+    private final WebClient webClient;
+    private final ParameterizedTypeReference<Product> productTypeReference = new ParameterizedTypeReference<Product>() {
+    };
+    private final ParameterizedTypeReference<List<Product>> productListTypeReference = new ParameterizedTypeReference<List<Product>>() {
+    };
     private final Resilience4jProductService resilience4jProductService;
 
     @Value("${rest.endpoint.fashion}")
@@ -35,26 +47,37 @@ public class ProductsController {
     @Value("${rest.endpoint.hotdeals}")
     private String urlHotDeals;
 
-    public ProductsController(ProductService productService, Resilience4jProductService resilience4jProductService) {
-        this.productService = productService;
+    public ProductsController(RestTemplateBuilder restTemplateBuilder, WebClient webClient,  Resilience4jProductService resilience4jProductService) {
+        this.restTemplate = restTemplateBuilder.setConnectTimeout(Duration.ofSeconds(2)).setReadTimeout(Duration.ofSeconds(2)).build();
+        this.restTemplateWithoutTimeout = restTemplateBuilder.build();
+        this.webClient = webClient;
         this.resilience4jProductService = resilience4jProductService;
     }
 
     @GetMapping
     public Products getProducts() {
         Products products = new Products();
-        products.setFashion(this.productService.getProduct(this.urlFashion));
-        products.setToys(this.productService.getProduct(this.urlToys));
-        products.setHotDeals(this.productService.getProduct(this.urlHotDeals));
+        products.setFashion(this.getProduct(this.urlFashion));
+        products.setToys(this.getProduct(this.urlToys));
+        products.setHotDeals(this.getProduct(this.urlHotDeals));
         return products;
     }
 
     @GetMapping("/exception")
     public Products getProductsBasicExceptionHandling() {
         Products products = new Products();
-        products.setFashion(this.productService.getProductBasicExceptionHandling(this.urlFashion));
-        products.setToys(this.productService.getProductBasicExceptionHandling(this.urlToys));
-        products.setHotDeals(this.productService.getProductBasicExceptionHandling(this.urlHotDeals));
+        products.setFashion(this.getProductBasicExceptionHandling(this.urlFashion));
+        products.setToys(this.getProductBasicExceptionHandling(this.urlToys));
+        products.setHotDeals(this.getProductBasicExceptionHandling(this.urlHotDeals));
+        return products;
+    }
+
+    @GetMapping("/timeout")
+    public Products getProductsWithTimeout() {
+        Products products = new Products();
+        products.setFashion(this.getProductWithTimeout(this.urlFashion));
+        products.setToys(this.getProductWithTimeout(this.urlToys));
+        products.setHotDeals(this.getProductWithTimeout(this.urlHotDeals));
         return products;
     }
 
@@ -73,20 +96,11 @@ public class ProductsController {
         return products;
     }
 
-    @GetMapping("/timeout")
-    public Products getProductsWithTimeout() {
-        Products products = new Products();
-        products.setFashion(this.productService.getProductWithTimeout(this.urlFashion));
-        products.setToys(this.productService.getProductWithTimeout(this.urlToys));
-        products.setHotDeals(this.productService.getProductWithTimeout(this.urlHotDeals));
-        return products;
-    }
-
     @GetMapping("/parallel")
     public Mono<Products> getProductsParallel() {
-        Mono<List<Product>> hotdeals = this.productService.getProductReactive("/products/hotdeals");
-        Mono<List<Product>> fashion = this.productService.getProductReactive("/products/fashion");
-        Mono<List<Product>> toys = this.productService.getProductReactive("/products/toys");
+        Mono<List<Product>> hotdeals = this.getProductReactive("/products/hotdeals");
+        Mono<List<Product>> fashion = this.getProductReactive("/products/fashion");
+        Mono<List<Product>> toys = this.getProductReactive("/products/toys");
 
         return Mono.zip(hotdeals, fashion, toys)
                 .flatMap(transformer -> Mono.just(new Products(transformer.getT1(), transformer.getT2(), transformer.getT3())));
@@ -94,9 +108,9 @@ public class ProductsController {
 
     @GetMapping({ "/circuitbreaker", "/cb", "/v2" })
     public Mono<Products> getProductsCircuitBreaker() {
-        Mono<List<Product>> hotdeals = this.productService.getProductReactive("/products/hotdeals/circuitbreaker");
-        Mono<List<Product>> fashion = this.productService.getProductReactive("/products/fashion/circuitbreaker");
-        Mono<List<Product>> toys = this.productService.getProductReactive("/products/toys/circuitbreaker");
+        Mono<List<Product>> hotdeals = this.getProductReactive("/products/hotdeals/circuitbreaker");
+        Mono<List<Product>> fashion = this.getProductReactive("/products/fashion/circuitbreaker");
+        Mono<List<Product>> toys = this.getProductReactive("/products/toys/circuitbreaker");
 
         return Mono.zip(hotdeals, fashion, toys)
                 .flatMap(transformer -> Mono.just(new Products(transformer.getT1(), transformer.getT2(), transformer.getT3())));
@@ -110,4 +124,34 @@ public class ProductsController {
         return ResponseEntity.ok().headers(headers).body(Collections.emptyList());
     }
 
+    private List<Product> getProduct(String url) {
+        return this.restTemplateWithoutTimeout.exchange(url, HttpMethod.GET, null, this.productListTypeReference).getBody();
+    }
+
+    private List<Product> getProductBasicExceptionHandling(String url) {
+        try {
+            return this.getProduct(url);
+        } catch (RestClientException e) {
+            log.error("RestClientException occurred when fetching products", e);
+            return Collections.emptyList();
+        }
+    }
+
+    private List<Product> getProductWithTimeout(String url) {
+        try {
+            return this.restTemplate.exchange(url, HttpMethod.GET, null, this.productListTypeReference).getBody();
+        } catch (RestClientException e) {
+            log.error("RestClientException occurred when fetching products", e);
+            return Collections.emptyList();
+        }
+    }
+
+    private Mono<List<Product>> getProductReactive(String uri) {
+        return this.webClient.get().uri(uri)
+                .retrieve()
+                .bodyToFlux(this.productTypeReference)
+                .collectList()
+                .flatMap(Mono::just)
+                .doOnError(throwable -> log.error("Error occurred", throwable));
+    }
 }
