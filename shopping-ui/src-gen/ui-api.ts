3ae9f360d3fd7fdeaa0ced316ad953b28ccf385d
Nils Wloka <nils@steadybit.com>
2023-01-05 12:37:19 +0100

    Add arm64 maven profile to support ARM images for M1 based deployments.

--- shopping-ui/src-gen/ui-api.ts
+++ shopping-ui/src-gen/ui-api.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2023-01-03 12:23:09.
+// Generated using typescript-generator version 3.1.1185 on 2023-01-05 12:30:08.
 
 export interface Product {
     id: string;
