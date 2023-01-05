d8e4fd19bb634925b14f90959664a44c405fa553
Johannes Edmeier <johannes.edmeier@steadybit.com>
2023-01-05 13:00:03 +0100

    chore: update to recent spring boot 2.7.7
    and remove hystrix (not supported anymore)

--- bestseller-toys/src/test/java/com/steadybit/demo/shopping/bestseller/toys/BestsellerToysApplicationTest.java
+++ bestseller-toys/src/test/java/com/steadybit/demo/shopping/bestseller/toys/BestsellerToysApplicationTest.java
@@ -4,12 +4,9 @@
 
 package com.steadybit.demo.shopping.bestseller.toys;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class BestsellerToysApplicationTest {
 
