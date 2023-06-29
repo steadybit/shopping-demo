c1fc2f54e347204287cf263dbd8e457f09253a1d
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2023-06-29 17:43:18 +0200

    fix: pushing wrong images

--- bestseller-toys/ci/buildAndPushDockerImages.bash
+++ bestseller-toys/ci/buildAndPushDockerImages.bash
@@ -8,6 +8,6 @@ docker buildx create --use
 docker buildx build \
   --platform "linux/arm64,linux/amd64" \
   --push \
-  -t "steadybit/bestseller-fashion:$tag" \
+  -t "steadybit/bestseller-toys:$tag" \
   --build-arg "JAR_FILE=target/*.jar" \
   .
