e97a09339e9617d0231206bd77c1f08792eda6ac
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2023-06-29 17:27:43 +0200

    chore: try multiarch images

--- order/ci/buildAndPushDockerImages.bash
+++ order/ci/buildAndPushDockerImages.bash
@@ -8,6 +8,6 @@ docker buildx create --use
 docker buildx build \
   --platform "linux/arm64,linux/amd64" \
   --push \
-  -t "steadybit/bestseller-toys:$tag" \
+  -t "steadybit/orders:$tag" \
   --build-arg "JAR_FILE=target/*.jar" \
   .
