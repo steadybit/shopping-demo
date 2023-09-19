c3107c0bb5214b07fac25557b2e13a7c4b9d54ec
Johannes Edmeier <johannes.edmeier@steadybit.com>
2023-09-19 12:26:02 +0200

    build: add missing 3rd party licenses to container images

--- gateway/ci/buildAndPushDockerImages.bash
+++ gateway/ci/buildAndPushDockerImages.bash
@@ -4,10 +4,12 @@ set -eo pipefail
 
 tag="$1"
 
+cp ../shopping-ui/target/licenses/THIRD-PARTY-UI.csv ./target/licenses/THIRD-PARTY-UI.csv
+
 docker buildx create --use
 docker buildx build \
   --platform "linux/arm64,linux/amd64" \
-  --push \
+  --push=false \
   -t "steadybit/gateway:$tag" \
   --build-arg "JAR_FILE=target/*.jar" \
   .
