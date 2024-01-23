f8aa6cd7b135d893f842cc9d7e544361580a77fd
Manuel Gerding <manuel@steadybit.com>
2024-01-23 11:36:11 +0100

    feat: Tagging docker images with additional branch-tag

--- bestseller-fashion/ci/buildAndPushDockerImages.bash
+++ bestseller-fashion/ci/buildAndPushDockerImages.bash
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+#
+# Copyright 2024 steadybit GmbH. All rights reserved.
+#
+
 set -eo pipefail
 
 tag="$1"
+branch="$2"
 
 docker buildx create --use
 docker buildx build \
   --platform "linux/arm64,linux/amd64" \
   --push \
   -t "steadybit/bestseller-fashion:$tag" \
+  -t "steadybit/bestseller-fashion:$branch" \
   --build-arg "JAR_FILE=target/*.jar" \
   .
