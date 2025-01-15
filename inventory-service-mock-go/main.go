0d721d377c6c753a273719fd6f4e5b17ffb0da17
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2025-01-15 11:51:41 +0100

    feat: implement inventory-service-mock as golang app

---
+++ inventory-service-mock-go/main.go
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2025 steadybit GmbH. All rights reserved.
+ */
+
+package main
+
+import (
+	"encoding/json"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/rs/zerolog/log"
+	"math/rand"
+	"net/http"
+)
+
+func isAvailable(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	response := rand.Float64() > 0.005
+	log.Debug().Str("Id", id).Str("response", "inventoryHandler").Msgf("response: %v", response)
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/inventory", isAvailable)
+	http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("ok"))
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to write response")
+			return
+		}
+	})
+	port := "8080"
+	println("Server running on port:", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		panic(err)
+	}
+}
