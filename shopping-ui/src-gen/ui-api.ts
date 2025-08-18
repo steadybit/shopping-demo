6c6904fc7f384a1dbd8d4a5c75fff4a4e691e003
Norbert Schneider <norbert@steadybit.com>
2025-08-18 12:55:30 +0200

    chore: regenerate package-lock

--- shopping-ui/src-gen/ui-api.ts
+++ shopping-ui/src-gen/ui-api.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2024-12-20 08:28:45.
+// Generated using typescript-generator version 3.2.1263 on 2025-08-18 12:51:24.
 
 export interface Order {
     id: string;
